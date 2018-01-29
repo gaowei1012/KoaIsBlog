2e857cf7072617492afe9ee7991437d1f1475893
gmw-zjw <gaomingwei2017@163.com>
2018-01-29 22:48:45 +0800

    config

---
+++ config/default.js
@@ -0,0 +1,16 @@
+const config = {
+    // 启动端口
+    port: 3000,
+
+    // 数据库链接配置
+    database: {
+        DATABASE: 'nodesql',
+        USERNAME: 'root',
+        PASSWORD: 'gaowei',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+
+}
+
+module.exports = config
