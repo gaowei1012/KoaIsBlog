82228821e17057c89a22ccb8b1d493e6539d337d
gmw-zjw <gaomingwei2017@163.com>
2018-01-30 17:13:01 +0800

    修改不能上传图片bug

--- routes/signup.js
+++ routes/signup.js
@@ -1,73 +1,63 @@
-/**
- *  注册模块
- */
 const router = require('koa-router')();
-const userModel = require('../lib/mysql');
-const md5 = require('md5');
-const checkNotLogin = require('../middlewares/check').checkNotLogin;
-const checkLogin = require('../middlewares/check').checkLogin;
+const userModel = require('../lib/mysql.js');
+const md5 = require('md5')
+const checkNotLogin = require('../middlewares/check.js').checkNotLogin
+const checkLogin = require('../middlewares/check.js').checkLogin
 const moment = require('moment');
-const fs  = require('fs');
-
+const fs = require('fs')
 // 注册页面
-router.get('/signup', async (ctx, next) => {
+router.get('/signup', async(ctx, next) => {
     await checkNotLogin(ctx)
     await ctx.render('signup', {
-        session: ctx.session
+        session: ctx.session,
     })
-});
-
-
+})
 // post 注册
-router.post('/signup', async (ctx, next) => {
+router.post('/signup', async(ctx, next) => {
+    //console.log(ctx.request.body)
     let user = {
         name: ctx.request.body.name,
         pass: ctx.request.body.password,
         repeatpass: ctx.request.body.repeatpass,
         avator: ctx.request.body.avator
     }
-
-    await userModel.findDataByName(user.name) 
+    await userModel.findDataByName(user.name)
         .then(async (result) => {
-            console.log(result);
-            if (result.lenght) {
+            console.log(result)
+            if (result.length) {
                 try {
                     throw Error('用户已经存在')
                 } catch (error) {
+                    //处理err
                     console.log(error)
                 }
-                // 用户已经存在
+                // 用户存在
                 ctx.body = {
                     data: 1
-                }
+                };;
+                
             } else if (user.pass !== user.repeatpass || user.pass === '') {
                 ctx.body = {
                     data: 2
-                }
+                };
             } else {
+                // ctx.session.user=ctx.request.body.name   
                 let base64Data = user.avator.replace(/^data:image\/\w+;base64,/, "");
                 let dataBuffer = new Buffer(base64Data, 'base64');
-                let getName = Number(Math.random().toString().substr(3).toString(36) + Date.now());
-
-                // 头像上传
-                await fs.writeFile('./public/images' + getName + '.png',  dataBuffer, err => {
-                    if (err) {
-                        throw err;
-                    }
-                    console.log('头像上传成功！')
-                });
-
-                // 插入数据库
+                let getName = Number(Math.random().toString().substr(3)).toString(36) + Date.now()
+                await fs.writeFile('./public/images/' + getName + '.png', dataBuffer, err => { 
+                    if (err) throw err;
+                    console.log('头像上传成功') 
+                });            
                 await userModel.insertData([user.name, md5(user.pass), getName, moment().format('YYYY-MM-DD HH:mm:ss')])
-                    .then(res => {
-                        console.log('注册成功', res);
+                    .then(res=>{
+                        console.log('注册成功',res)
+                        //注册成功
                         ctx.body = {
                             data: 3
-                        }
+                        };
                     })
-
             }
         })
 })
-
 module.exports = router
