3ed6b8817aa2fa43cc748ad427d40d0c613b716b
gmw-zjw <gaomingwei2017@163.com>
2018-01-30 16:20:22 +0800

    登录逻辑实现

--- routes/signin.js
+++ routes/signin.js
@@ -4,11 +4,34 @@ const md5 = require('md5');
 const checkNotLogin = require('../middlewares/check').checkNotLogin;
 const checkLogin = require('../middlewares/check').checkLogin;
 
+// 登录
 router.get('/signin', async (ctx, next) => {
     await checkNotLogin(ctx);
     await ctx.render('signin', {
         session: ctx.session,
     })
+})
+
+// 登录实现
+router.post('/signin', async (ctx, next) => {
+    console.log(ctx.request.body);
+    let name = ctx.request.body.name;
+    let pass = ctx.request.body.password;
+
+    await userModel.findDataByName(name)
+        .then(result => {
+            let res = result;
+            if (name === res[0]['name'] && md5(pass) === res[0]['pass']) {
+                ctx.body = true;
+                ctx.session.user = res[0]['name']
+                ctx.session.id = res[0]['id']
+            } else {
+                ctx.body = false;
+                console.log('用户名或密码有误！');
+            }
+        }).catch(err => {
+            console.log(err);
+        })
 })
 
 module.exports = router
